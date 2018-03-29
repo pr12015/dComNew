7e9baa4b2222ad8ea8a2fe40031d9af61dde05e7
Stefan Stipanovic <s_stefan@outlook.com>
2018-03-29 14:22:27 +0200

    Add project files.

---
+++ dCom/Acquisition/Acquisitor.cs
@@ -0,0 +1,91 @@
+﻿using dCom.Configuration;
+using dCom.Connection;
+using dCom.Modbus;
+using dCom.Modbus.FunctionParameters;
+using dCom.Modbus.ModbusFunctions;
+using dCom.ViewModel;
+using System;
+using System.Threading;
+
+namespace dCom.Acquisition
+{
+	public class Acquisitor : IDisposable
+	{
+		private AutoResetEvent acquisitionTrigger;
+		private FunctionExecutor commandExecutor;
+		private Thread acquisitionWorker;
+		private IStateUpdater stateUpdater;
+		private bool acquisitionStopSignal = true;
+        private int DO_REG_sekunde = 0;
+        private int HR_INT_sekunde = 0;
+
+        public Acquisitor(AutoResetEvent acquisitionTrigger, FunctionExecutor commandExecutor, IStateUpdater stateUpdater)
+		{
+			this.stateUpdater = stateUpdater;
+			this.acquisitionTrigger = acquisitionTrigger;
+			this.commandExecutor = commandExecutor;
+			this.InitializeAcquisitionThread();
+			this.StartAcquisitionThread();
+		}
+
+		#region Private Methods
+
+		private void InitializeAcquisitionThread()
+		{
+			this.acquisitionWorker = new Thread(Acquisition_DoWork);
+			this.acquisitionWorker.Name = "Acquisition thread";
+		}
+
+		private void StartAcquisitionThread()
+		{
+			acquisitionWorker.Start();
+		}
+
+		/// <summary>
+		/// Acquisition thread
+		///		Awaits for trigger;
+		///		After configured period send appropriate command to MdbSim for each point type
+		/// </summary>
+		private void Acquisition_DoWork()
+		{
+            while (true)
+            {
+                try
+                {
+                    acquisitionTrigger.WaitOne();
+                    DO_REG_sekunde++;
+                    
+                    HR_INT_sekunde++;
+                    if (DO_REG_sekunde == ConfigReader.Instance.GetAcquisitionInterval("DigOut"))
+                    {
+                        ModbusReadCommandParameters p = new ModbusReadCommandParameters(6, (byte)ModbusFunctionCode.READ_COILS, ConfigReader.Instance.GetStartAddress("DigOut"), ConfigReader.Instance.GetNumberOfRegisters("DigOut")); ;
+                        ModbusFunction fn = FunctionFactory.CreateModbusFunction(p);
+                        this.commandExecutor.EnqueueCommand(fn);
+                        DO_REG_sekunde = 0;
+                    }
+                    
+                    if (HR_INT_sekunde == ConfigReader.Instance.GetAcquisitionInterval("AnaOut"))
+                    {
+                        ModbusReadCommandParameters p = new ModbusReadCommandParameters(6, (byte)ModbusFunctionCode.READ_HOLDING_REGISTERS, ConfigReader.Instance.GetStartAddress("AnaOut"), ConfigReader.Instance.GetNumberOfRegisters("AnaOut")); ;
+                        ModbusFunction fn = FunctionFactory.CreateModbusFunction(p);
+                        this.commandExecutor.EnqueueCommand(fn);
+                        HR_INT_sekunde = 0;
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    string message = $"{ex.TargetSite.ReflectedType.Name}.{ex.TargetSite.Name}: {ex.Message}";
+                    stateUpdater.LogMessage(message);
+                }
+            }
+        }
+
+		#endregion Private Methods
+
+		public void Dispose()
+		{
+			acquisitionStopSignal = false;
+		}
+	}
+}
---
+++ dCom/Modbus/ModbusFunctions/ReadHoldingRegistersFunction.cs
@@ -0,0 +1,68 @@
+﻿using dCom.Modbus.FunctionParameters;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+
+namespace dCom.Modbus.ModbusFunctions
+{
+	public class ReadHoldingRegistersFunction : ModbusFunction
+	{
+		public ReadHoldingRegistersFunction(ModbusCommandParameters commandParameters) : base(commandParameters)
+		{
+			CheckArguments(MethodBase.GetCurrentMethod(), typeof(ModbusReadCommandParameters));
+		}
+
+		/// <inheritdoc />
+		public override byte[] PackRequest()
+		{
+            ModbusReadCommandParameters mdmReadCommParams = this.CommandParameters as ModbusReadCommandParameters;
+            byte[] mdbRequest = new byte[12];
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmReadCommParams.TransactionId)), 0, mdbRequest, 0, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmReadCommParams.ProtocolId)), 0, mdbRequest, 2, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmReadCommParams.Length)), 0, mdbRequest, 4, 2);
+            mdbRequest[6] = mdmReadCommParams.UnitId;
+            mdbRequest[7] = mdmReadCommParams.FunctionCode;
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmReadCommParams.StartAddress)), 0, mdbRequest, 8, 2);
+            Buffer.BlockCopy(BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)mdmReadCommParams.Quantity)), 0, mdbRequest, 10, 2);
+            return mdbRequest;
+        }
+
+		/// <inheritdoc />
+		public override Dictionary<Tuple<PointType, ushort>, ushort> ParseResponse(byte[] response)
+		{
+            ModbusReadCommandParameters mdmReadCommParams = this.CommandParameters as ModbusReadCommandParameters;
+            Dictionary<Tuple<PointType, ushort>, ushort> dic = new Dictionary<Tuple<PointType, ushort>, ushort>();
+            int kolicina = response[8];
+            int prosao = 0;
+            ushort value = 0;
+            int broo = 0;
+
+
+            for (int i = 0; i < kolicina; i++)
+            {
+                ushort value1 = (ushort)(response[9 + i]);
+                for (int j = 0; j < 8; j++)
+                {
+                    if (prosao != mdmReadCommParams.Quantity)
+                    {
+                        byte[] r = new byte[2];
+
+                        r[1] = response[9 + broo];
+                        r[0] = response[10 + broo];
+
+                        value = (ushort)BitConverter.ToInt16(r, 0);
+
+                        dic.Add(new Tuple<PointType, ushort>(PointType.ANALOG_OUTPUT, (ushort)(mdmReadCommParams.StartAddress + prosao)), value);
+
+                        prosao++;
+                        broo += 2;
+                    }
+                    else break;
+                }
+            }
+
+            return dic;
+        }
+	}
+}
